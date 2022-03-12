c05661602da296ff25c538683e090e384a40eca4
Weiwei Liang <liang.wen.wei.903@gmail.com>
2022-03-11 23:40:41 -0800

    reversed message on load & fix bug with new message not updating app

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -62,9 +62,9 @@ const Home = ({ user, logout }) => {
     });
   };
 
-  const postMessage = (body) => {
+  const postMessage =async (body) => {
     try {
-      const data = saveMessage(body);
+      const data =await saveMessage(body);
 
       if (!body.conversationId) {
         addNewConvo(body.recipientId, data.message);
@@ -106,15 +106,20 @@ const Home = ({ user, logout }) => {
         setConversations((prev) => [newConvo, ...prev]);
       }
 
-      conversations.forEach((convo) => {
-        if (convo.id === message.conversationId) {
-          convo.messages.push(message);
-          convo.latestMessageText = message.text;
-        }
-      });
-      setConversations(conversations);
+      setConversations((prev) =>
+        prev.map((convo) =>{
+          if (convo.id === message.conversationId) {
+            const convoCopy = { ...convo};
+            convoCopy.messages.push(message);
+            convoCopy.latestMessageText = message.text;
+            return convoCopy;
+          }else{
+            return convo
+          }
+        })
+      );
     },
-    [setConversations, conversations]
+    [setConversations]
   );
 
   const setActiveChat = (username) => {
@@ -183,6 +188,9 @@ const Home = ({ user, logout }) => {
     const fetchConversations = async () => {
       try {
         const { data } = await axios.get('/api/conversations');
+        data.map((convo) =>(
+          convo.messages.reverse()
+        ))
         setConversations(data);
       } catch (error) {
         console.error(error);
--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,8 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+const db = new Sequelize('messenger','msgrdev','msgrdev', {
+  host:'localhost',
+  dialect:'postgres',
   logging: false
 });
 
