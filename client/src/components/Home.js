7bb76728e354f3788d770c60747229ca567acacc
Weiwei Liang <liang.wen.wei.903@gmail.com>
2022-03-13 13:46:05 -0700

    revert unrelated commit to ticket & change addNewConvo as commented on PR

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -80,17 +80,21 @@ const Home = ({ user, logout }) => {
 
   const addNewConvo = useCallback(
     (recipientId, message) => {
-      const convCPY = [...conversations];
-      convCPY.forEach((convo) => {
-        if (convo.otherUser.id === recipientId) {
-          convo.messages.push(message);
-          convo.latestMessageText = message.text;
-          convo.id = message.conversationId;
-        }
-      });
-      setConversations(convCPY);
+      setConversations((prev) =>(
+        prev.map( (convo) =>{
+          if(convo.otherUser.id === recipientId){
+            const convoCopy = {...convo};
+            convoCopy.messages.push(message);
+            convoCopy.latestMessageText = message.text;
+            convoCopy.id = message.conversationId;
+            return convoCopy;
+          }else{
+            return convo;
+          }
+        })
+      ));
     },
-    [setConversations, conversations]
+    [setConversations]
   );
 
   const addMessageToConversation = useCallback(
