81fe98474e5d8fa0adaad1ef4c02ad218cc43b53
Weiwei Liang <liang.wen.wei.903@gmail.com>
2022-03-14 02:43:18 -0700

    handle state immutability

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -84,7 +84,7 @@ const Home = ({ user, logout }) => {
         prev.map( (convo) =>{
           if(convo.otherUser.id === recipientId){
             const convoCopy = {...convo};
-            convoCopy.messages = [...convoCopy,message];
+            convoCopy.messages = [...convoCopy.messages,message];
             convoCopy.latestMessageText = message.text;
             convoCopy.id = message.conversationId;
             return convoCopy;
@@ -115,7 +115,7 @@ const Home = ({ user, logout }) => {
         prev.map((convo) =>{
           if (convo.id === message.conversationId) {
             const convoCopy = { ...convo};
-            convoCopy.messages = [...convoCopy, message]
+            convoCopy.messages = [...convoCopy.messages, message]
             convoCopy.latestMessageText = message.text;
             return convoCopy;
           }else{
